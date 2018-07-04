9d433246426121000ab802c57220f8a417cf1223
George Ioakeimidis <giorgosioak95@gmail.com>
2018-07-04 21:02:48 +0300

    noto font

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ fi
 
 aurman -S i3-gaps i3-wm i3blocks i3lock i3status compton feh \
 dmenu lxappearance gnome-screenshot gnome-terminal termite \
-nerd-fonts-source-code-pro
+nerd-fonts-source-code-pro noto-fonts
 
 echo
 echo '---------------------------------------------'
